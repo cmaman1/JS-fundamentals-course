3a816f7a5eb09fd8921346759357530f5ec44f3f
csmamani <mamaniccarolina@gmail.com>
2020-04-19 17:18:10 -0300

    Clase 32. Promesas.

---
+++ Clase32/clase32.js
@@ -0,0 +1,36 @@
+const API_URL = 'https://pokeapi.co/api/v2/';
+const POKEMON_URL = 'pokemon/:id';
+const options = { crossdomain: true };
+
+
+function obtenerPokemon(id) {
+    return new Promise((resolve, reject) => {
+        const url = `${API_URL}${POKEMON_URL.replace(':id', id)}`;
+
+        $
+        .get(url, options, function(data) {
+            resolve(data);
+        })
+        .fail( () => {
+            reject(id)
+        });
+    }); 
+}
+
+function onError(id) {
+    console.log(`Sucedio un error al obtener el personaje ${id}`)
+}
+
+for (var i = 0; i < 999; i++) {
+    obtenerPokemon(i)
+        .then(function (pokemon){
+        console.log(`El personaje ${pokemon.id} es ${pokemon.name}`)
+        })
+        .catch(onError);
+}
+
+// obtenerPokemon(1)
+//     .then(function (pokemon){
+//         console.log(`El personaje 1 es ${pokemon.name}`)
+//     })
+//     .catch(onError);
